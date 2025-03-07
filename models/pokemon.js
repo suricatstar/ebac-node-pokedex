d9cb18ed1ae9ae79c98bdd8684149abd2f3d54c8
Cauê Tamiarana <caue.tamiarana@gmail.com>
2025-03-07 01:10:21 -0300

    schema de pokemon feito

---
+++ models/pokemon.js
@@ -0,0 +1,44 @@
+const { NOMEM } = require('dns');
+const {Schema} = require('mongoose');
+const { type } = require('os');
+
+const Pokemon = new Schema({
+    id: {
+        type: Number,
+        required: true,
+    },
+    nome:{
+        type: String,
+        required: true,
+    },
+    altura:{
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    peso:{
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    imagem:{
+        type: String,
+        required: true,
+        validate:{
+            validator: (valor) => {
+                return valor && valor.startsWith('http')
+            },
+            message: () => 'A imagem deve ser uma URL absoluta'
+        }
+    },
+    ataques: {
+        type: String,
+        required: true,
+    },
+    estatisticas:{
+        type : Object,
+        required: true,
+    }
+}); 
+
+module.exports = Pokemon;

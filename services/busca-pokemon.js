60b8204f690e533e016b145bc965374f9699232b
Cauê Tamiarana <caue.tamiarana@gmail.com>
2025-03-20 14:53:30 -0300

    feito a busca pokemon

---
+++ services/busca-pokemon.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+const buscaInfoPokemon = (PokeId) => {
+    return new Promise((resolve, reject) => {
+
+        const url = `https://pokeapi.co/api/v2/pokemon/${PokeId}`;
+    
+        axios.get(url).then(resultado => {
+            const data = resultado.data;
+    
+            const id = data.id;
+            const nome = data.name;
+            const peso = data.weight;
+            const altura = data.height;
+            const imagem = data.sprites.other['official-artwork'].front_default;
+            const ataques = data.abilities.map(a => a.ability.name).join(", ");
+    
+            const estatisticas = {};
+    
+            data.stats.forEach((estatistica) => {
+                estatisticas[estatistica.stat.name] = estatistica.base_stat;
+            })
+
+
+            resolve({
+                id,
+                nome,
+                peso,
+                altura,
+                imagem,
+                ataques,
+                estatisticas
+            })
+    
+        }).catch((e) => reject(e));
+    });
+
+}
+
+module.exports = buscaInfoPokemon;

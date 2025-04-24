0fafce8f998b71188c7eb27580ec364f450c3daa
Cauê Tamiarana <caue.tamiarana@gmail.com>
2025-04-24 01:11:55 -0300

    try catch

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -1,38 +1,38 @@
-const express = require('express');
+const express = require("express");
 
-const { Pokemon } = require('../models');
+const { Pokemon } = require("../models");
 
 const router = express.Router();
 
-router.get('/', (_req, res) => {
-    Pokemon.find().then(pokemons => {
+router.get("/", async (_req, res) => {
+  const pokemons = await Pokemon.find();
 
-        pokemons.forEach(pokemon => {
-            const dataIso = pokemon._id.getTimestamp().toISOString();
-            const dataInfo = dataIso.split('T')[0];
-            const dataEmPedacos = dataInfo.split('-');
+  pokemons.forEach((pokemon) => {
+    const dataIso = pokemon._id.getTimestamp().toISOString();
+    const dataInfo = dataIso.split("T")[0];
+    const dataEmPedacos = dataInfo.split("-");
 
-            pokemon.capturadoEm = `${dataEmPedacos[2]}/${dataEmPedacos[1]}/${dataEmPedacos[0]}`;
-        });
+    pokemon.capturadoEm = `${dataEmPedacos[2]}/${dataEmPedacos[1]}/${dataEmPedacos[0]}`;
+  });
 
-        res.render('paginas/pokemons/index', {
-            pokemons,
-        })
-    });
+  res.render("paginas/pokemons/index", {
+    pokemons,
+  });
 });
 
-router.get('/:id', (req, res) => {
-    Pokemon.findOne({_id: req.params.id}).then(pokemon => {
-        res.render('paginas/pokemons/show', {
-            pokemon,
-            message: req.query.message,
-        });
-    }).catch(e => {
-        res.status(404).render('paginas/erro', {
-            mensagem: 'Pokemon não encontrado !!',
-            erro: {},
-        })
+router.get("/:id", async (req, res) => {
+  try {
+    const pokemon = await Pokemon.findOne({ _id: req.params.id });
+    res.render("paginas/pokemons/show", {
+      pokemon,
+      message: req.query.message,
     });
+  } catch (e) {
+    res.status(404).render("paginas/erro", {
+      mensagem: "Pokemon não encontrado !!",
+      erro: {},
+    });
+  }
 });
 
 module.exports = router;
